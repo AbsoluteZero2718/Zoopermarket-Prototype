8e886b51914b78faf304cd9e51c3c26b51afc949
Mattios <matthew.cabusay@ciit.edu.ph>
2026-03-16 18:05:19 +0800

    Zoopermarket v6
    > added pause and audio controls

---
+++ Assets/Scripts/PauseManager.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseManager : MonoBehaviour
+{
+    [SerializeField] private GameObject pauseUI;
+
+    private bool isGamePaused;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            PauseGame();
+        }
+    }
+
+    public void PauseGame()
+    {
+        isGamePaused = !isGamePaused;
+        Time.timeScale = isGamePaused ? 0f : 1f;
+        pauseUI.SetActive(isGamePaused);
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1;
+        isGamePaused = false;
+        pauseUI.SetActive(false);
+    }
+
+    public void GoToMenu()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("MainMenu");
+    }
+}
